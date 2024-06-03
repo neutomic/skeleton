9e29a7e661f2ebaf3ae2e5690d96030eb495bf2e
azjezz <azjezz@protonmail.com>
2024-06-03 22:42:32 +0100

    chore: update the default template
    Signed-off-by: azjezz <azjezz@protonmail.com>

--- src/Handler/IndexHandler.php
+++ src/Handler/IndexHandler.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Handler;
 
+use Neu\Component\DependencyInjection\Project;
 use Neu\Component\Http\Message\Method;
 use Neu\Component\Http\Message\RequestInterface;
 use Neu\Component\Http\Message\ResponseInterface;
@@ -18,20 +19,32 @@ use Twig\Error\Error as TwigError;
  * The index handler used to render the index page.
  */
 #[Route(name: 'index', pattern: '/', methods: [Method::Get])]
-final readonly class IndexHandler implements HandlerInterface
+final class IndexHandler implements HandlerInterface
 {
+    /**
+     * The project instance.
+     */
+    private Project $project;
+
     /**
      * The twig environment used to render templates.
      */
     private Environment $twig;
 
     /**
+     * The server counter.
+     */
+    private int $serverCounter = 0;
+
+    /**
      * Create a new instance of the handler.
      *
+     * @param Project $project The project instance.
      * @param Environment $twig The twig environment used to render templates.
      */
-    public function __construct(Environment $twig)
+    public function __construct(Project $project, Environment $twig)
     {
+        $this->project = $project;
         $this->twig = $twig;
     }
 
@@ -42,12 +55,17 @@ final readonly class IndexHandler implements HandlerInterface
      */
     public function handle(Context $context, RequestInterface $request): ResponseInterface
     {
+        $serverCounter = ++$this->serverCounter;
+
         $session = $request->getSession();
         $counter = $session->update('counter', static function(?int $count): int {
             return (null !== $count) ? ($count + 1) : 1;
         });
 
         $content = $this->twig->render('index.html.twig', [
+            'project' => $this->project,
+            'context' => $context,
+            'serverCounter' => $serverCounter,
             'counter' => $counter,
         ]);
 
