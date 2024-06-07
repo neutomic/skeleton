88cef4784e7c02b12069eb3cfeb5c63e69f33d29
azjezz <azjezz@protonmail.com>
2024-06-07 05:31:39 +0100

    chore: update to the latest neu version
    Signed-off-by: azjezz <azjezz@protonmail.com>

--- src/main.php
+++ src/main.php
@@ -7,37 +7,102 @@ namespace App;
 use Neu;
 use Neu\Component\DependencyInjection\ContainerBuilder;
 use Neu\Component\DependencyInjection\Project;
+use Neu\Framework\EngineInterface;
 use Psl\Env;
 use Psl\SecureRandom;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-(static function(): void {
-    // Load the project secret from the environment.
+/* |----------------------------------------| */
+/* | Run the engine inside a closure        | */
+/* | to avoid leaking variables to the      | */
+/* | global scope.                          | */
+/* |----------------------------------------| */
+(static function (): void {
+    /* |----------------------------------------| */
+    /* | Retrieve the project secret.           | */
+    /* |----------------------------------------| */
     $secret = Env\get_var('PROJECT_SECRET');
+    $generated = false;
+
+    /* |----------------------------------------| */
+    /* | Check if the secret is missing.        | */
+    /* |----------------------------------------| */
     if (null === $secret) {
-        // Generate a random secret if it's not set.
+        /* |----------------------------------------| */
+        /* | Generate a new secret.                 | */
+        /* |----------------------------------------| */
         $secret = SecureRandom\string(32);
 
-        // Set the secret as an environment variable.
+        /* |----------------------------------------| */
+        /* | Store the secret in the environment.   | */
+        /* |----------------------------------------| */
         Env\set_var('PROJECT_SECRET', $secret);
+
+        /* |----------------------------------------| */
+        /* | Mark the secret as generated.          | */
+        /* |----------------------------------------| */
+        $generated = true;
     }
 
-    // Create a new project instance.
-    $project = Project::create($secret, __DIR__ . '/../', __FILE__);
+    /* |----------------------------------------| */
+    /* | Create a project instance.             | */
+    /* |----------------------------------------| */
+    $project = Project::create(secret: $secret, directory: __DIR__ . '/../', entrypoint: __FILE__);
 
-    // Create the container.
-    $container = ContainerBuilder::create($project, [], [
-        new Neu\Component\Configuration\DependencyInjection\ConfigurationExtension(),
+    /* |----------------------------------------| */
+    /* | Check if the project is in production. | */
+    /* |----------------------------------------| */
+    if ($project->mode->isProduction() && $generated) {
+        /* |----------------------------------------| */
+        /* | Throw an exception if the project      | */
+        /* | secret has been generated in           | */
+        /* | production mode.                       | */
+        /* |----------------------------------------| */
+        throw new Neu\Framework\Exception\RuntimeException('The project secret has been generated in production mode.');
+    }
+
+    /* |----------------------------------------| */
+    /* | Create a container builder.            | */
+    /* |----------------------------------------| */
+    $builder = ContainerBuilder::create($project);
+
+    /* |----------------------------------------| */
+    /* | Add configuration resources to the     | */
+    /* | container builder.                     | */
+    /* |----------------------------------------| */
+    $builder->addConfigurationResource(__DIR__ . '/../config');
+    $builder->addConfigurationResource(__DIR__ . '/../config/' . $project->mode->value . '/');
+
+    /* |----------------------------------------| */
+    /* | Add extensions to the container        | */
+    /* | builder.                               | */
+    /* |----------------------------------------| */
+    $builder->addExtensions([
         new Neu\Bridge\Monolog\DependencyInjection\MonologExtension(),
         new Neu\Framework\DependencyInjection\FrameworkExtension(),
         new Neu\Bridge\Twig\DependencyInjection\TwigExtension(),
-    ])->build();
+    ]);
 
-    // Retrieve the engine from the container.
-    $engine = $container->getTyped(Neu\Framework\EngineInterface::class, Neu\Framework\EngineInterface::class);
+    /* |----------------------------------------| */
+    /* | Add paths to the container builder for | */
+    /* | auto-discovery.                        | */
+    /* |----------------------------------------| */
+    $builder->addPathForAutoDiscovery(__DIR__);
 
-    // Run the engine.
+    /* |----------------------------------------| */
+    /* | Build the container.                   | */
+    /* |----------------------------------------| */
+    $container = $builder->build();
+
+    /* |----------------------------------------| */
+    /* | Retrieve the engine from the           | */
+    /* | container.                             | */
+    /* |----------------------------------------| */
+    $engine = $container->getTyped(EngineInterface::class, EngineInterface::class);
+
+    /* |----------------------------------------| */
+    /* | Run the engine.                        | */
+    /* |----------------------------------------| */
     $engine->run();
 })();
-
